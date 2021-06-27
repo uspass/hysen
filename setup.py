e6e8a81a80cdfd10283a533d7c184a59992bf010
uspass <42880649+uspass@users.noreply.github.com>
2021-06-27 08:24:40 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='hysen',
+    version='0.4.9',
+    author='us',
+    description='Python API for controlling Hysen thermostats',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/uspass/hysen',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=['broadlink==0.17.0'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+    ],
+)
